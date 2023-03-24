39a000c661c917752af3dbf2faa4f70f9053f059
kajal-2011 <kajalrekha888@gmail.com>
2023-03-24 16:44:45 +0600

    get static paths

---
+++ pages/post/index.js
@@ -0,0 +1,26 @@
+import PostItem from "@/components/post-items";
+
+const Posts = ({ posts }) => {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-4xl">Posts</h1>
+
+      {posts.map((post) => (
+        <PostItem post={post} key={post.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Posts;
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  return {
+    props: {
+      posts: data,
+    },
+  };
+}
