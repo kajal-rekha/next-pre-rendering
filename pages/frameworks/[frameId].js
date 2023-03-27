ab6e7b4f450cf7c64a889e930c6252e529058f4a
kajal-2011 <kajalrekha888@gmail.com>
2023-03-27 11:15:20 +0600

    server-side rendering done

---
+++ pages/frameworks/[frameId].js
@@ -0,0 +1,28 @@
+const FrameworkDetails = ({ framework }) => {
+  return (
+    <div>
+      <h1>
+        {" "}
+        {framework.id} - {framework.title}
+      </h1>
+      <p>{framework.core}</p>
+    </div>
+  );
+};
+
+export default FrameworkDetails;
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+
+  const res = await fetch(`http://localhost:4000/frameworks/${params.frameId}`);
+  const data = await res.json();
+
+  console.log("getServerSideProps()  ran...");
+
+  return {
+    props: {
+      framework: data,
+    },
+  };
+}
